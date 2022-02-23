d7be61e9ef1030a3a01d7e536fd14b2020b4e083
Robert Simon <robfara@gmail.com>
2022-02-22 21:21:37 -0500

    Version 2 - Practice All Books

--- src/main/java/com/codingdojo/book/controllers/BookController.java
+++ src/main/java/com/codingdojo/book/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.codingdojo.book.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codingdojo.book.models.Book;
 import com.codingdojo.book.services.BookService;
@@ -22,4 +25,10 @@ public class BookController {
 		return "show.jsp";
 	}
 	
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "index.jsp";
+    }	
 }
